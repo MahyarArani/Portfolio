e3a82699c830ebcecc111b8bdcd925e167696950
Mahyar Arani <mahyar@vt.edu>
2026-04-15 23:15:59 -0400

    Configure base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://MahyarArani.github.io',
+  base: '/Portfolio', // Important: this must match your GitHub repository name!
   vite: {
     plugins: [tailwindcss()]
   }
