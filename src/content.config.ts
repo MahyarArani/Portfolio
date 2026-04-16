cac32e73338a964eeb1fbf631aacb7ba29ad34db
Mahyar Arani <mahyar@vt.edu>
2026-04-15 23:09:07 -0400

    Fix Astro 6 compatibility issues

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
-
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const projects = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/content/projects" }),
 	schema: z.object({
 		title: z.string(),
 		date: z.string().optional(),
@@ -10,5 +11,4 @@ const projects = defineCollection({
 	}),
 });
 
-export { projects };
 export const collections = { projects };
